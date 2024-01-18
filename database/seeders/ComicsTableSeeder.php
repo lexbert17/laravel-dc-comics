6423b6c53524ca7c90bcadbee091a6991b5f4ceb
lexbert17 <lexbertlazo@gmail.com>
2024-01-18 21:21:04 +0100

    add: creazione controller(crud) e inserimento della sua route

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,13 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics_list = config('comics');
+        foreach($comics_list as $comic_list);
+        $new_comic = new Comic();
+        $new_comic->title = $comic_list['series'];
+        $new_comic->title = $comic_list['thumb'];
+        $new_comic->title = $comic_list['price'];
+        $new_comic->title = $comic_list['type'];
+        $new_comic-> save();
     }
 }
